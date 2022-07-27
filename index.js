d4206bed8006faa3ffc2783bc5dbb84a70ed7899
L-YSF <84968920+useraccount111@users.noreply.github.com>
2022-07-27 18:35:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function add(addend1, addend2) {
+const add = (addend1,addend2) => {
 	let sum = addend1 + addend2;
 	return sum;
 }
 
-function subtract(minuend, subtrahend) {
+const subtract = (minuend,subtrahend) => {
 	let difference = minuend - subtrahend;
 	return difference;
 }
 
-function multiply(multiplicand, multiplier) {
+const multiply = (multiplicand, multiplier) => {
 	let product = multiplicand * multiplier;
 	return product;
 }
 
-function divide(dividend, divisor) {
+const divide = (dividend,divisor) => {
 	let quotient = dividend / divisor;
 	return quotient;
 }
